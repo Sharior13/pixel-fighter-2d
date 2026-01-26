2ca7ac5b840af8fafba27feebddc10bd52d80bf3
sharior13 <sauravshrestha648@gmail.com>
2026-01-26 14:27:59 +0545

    Added title screen

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,22 @@
 import { initializeRender, canvas } from "./render.js";
 
-const titleForm = document.getElementById('title-form');
-const playerName = document.getElementById('player-name');
+const titleDiv = document.getElementById('title-screen');
+// const titleForm = document.getElementById('title-form');
+// const playerName = document.getElementById('player-name');
 
 const titleScreen = ()=>{
-
-    titleForm.style = `left:${canvas.width/2 - 250}px; top:${-canvas.height/2 + 55}px`;
-    titleForm.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        startGame();
+    const button = document.querySelector('.btn');
+    button.addEventListener('click', ()=>{
+        switch(button.id){
+            case "quick-start-btn":
+                startGame();
+        }
     });
 };
 const startGame = ()=>{
-    titleForm.style.display = 'none';
+    titleDiv.style.display = 'none';
+    initializeRender();
 };
 titleScreen();
-initializeRender();
 
 export { titleScreen };
